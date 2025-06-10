4ac285c77029b86393ecc61216f80f6f3052860d
Arabella Blandin <blandinqa@gmail.com>
2025-06-09 18:05:39 -0600

    Juego Semanal y Juego Mensual

---
+++ PII_2025_II_1P_LABORATORIO1/JuegoDiario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PII_2025_II_1P_LABORATORIO1
+{
+    public partial class JuegoDiario : Form
+    {
+        public JuegoDiario()
+        {
+            InitializeComponent();
+        }
+    }
+}
